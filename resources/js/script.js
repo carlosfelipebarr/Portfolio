0e6696113e5c12191b7583b5563d187d370138c7
localiza-descarte <89545100+felipecarvalhobarr@users.noreply.github.com>
2021-12-26 23:40:07 -0300

    Atualização do Front-End

--- resources/js/script.js
+++ resources/js/script.js
@@ -3,3 +3,23 @@ function onClickMenu(){
     document.getElementById("nav").classList.toggle("change");
     document.getElementById("menu-bg").classList.toggle("change-bg");
 }
+
+// Criando a variavel mybutton pegando o id do button no html
+var mybutton = document.getElementById("btn-top");
+
+// Quando rolar a página 25px do topo para baixo, o botão será exibido
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 25 || document.documentElement.scrollTop > 25) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// // quando clicar no botão, a página rola para o topo do documento
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
