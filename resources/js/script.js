5db6bf0ff9e0686ba033c6dcf5447a70a5b984ae
localiza-descarte <89545100+felipecarvalhobarr@users.noreply.github.com>
2022-01-05 19:40:39 -0300

    Atualização do Front-End

--- resources/js/script.js
+++ resources/js/script.js
@@ -7,7 +7,7 @@ function onClickMenu(){
 // Criando a variavel mybutton pegando o id do button no html
 var mybutton = document.getElementById("btn-top");
 
-// Quando rolar a página 25px do topo para baixo, o botão será exibido
+// Quando rolar a página 28px do topo para baixo, o botão será exibido
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
