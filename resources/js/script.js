537c5b4f6167ebb0dff25684d362ee717e284fa8
felipecarvalhobarr <felipecarvalhobarr@gmail.com>
2022-01-21 14:21:11 -0300

    Atualização do Front-End

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,20 +1,52 @@
+// Função para o ativar o menu hamburguer ao clickar 
 function onClickMenu(){
     document.getElementById("menu").classList.toggle("change");
     document.getElementById("nav").classList.toggle("change");
     document.getElementById("menu-bg").classList.toggle("change-bg");
 }
 
-// Criando a variavel mybutton pegando o id do button no html
-var mybutton = document.getElementById("btn-top");
+// Constante para o selector .btnMenu
+const btnMenu1 = document.querySelector('.btnMenu1')
+const btnMenu2 = document.querySelector('.btnMenu2')
+const btnMenu3 = document.querySelector('.btnMenu3')
+const btnMenu4 = document.querySelector('.btnMenu4')
 
-// Quando rolar a página 28px do topo para baixo, o botão será exibido
+// Evento para fechar o menu após o click nos botões
+btnMenu1.addEventListener('click', function() {
+  document.getElementById("menu").classList.toggle("change");
+  document.getElementById("nav").classList.toggle("change");
+  document.getElementById("menu-bg").classList.toggle("change-bg");
+})
+
+btnMenu2.addEventListener('click', function() {
+  document.getElementById("menu").classList.toggle("change");
+  document.getElementById("nav").classList.toggle("change");
+  document.getElementById("menu-bg").classList.toggle("change-bg");
+})
+
+btnMenu3.addEventListener('click', function() {
+  document.getElementById("menu").classList.toggle("change");
+  document.getElementById("nav").classList.toggle("change");
+  document.getElementById("menu-bg").classList.toggle("change-bg");
+})
+
+btnMenu4.addEventListener('click', function() {
+  document.getElementById("menu").classList.toggle("change");
+  document.getElementById("nav").classList.toggle("change");
+  document.getElementById("menu-bg").classList.toggle("change-bg");
+})
+
+// Criando a variavel mybutton pegando o id do button no html
+var btnTop = document.getElementById("btn-top");
+
+// Quando rolar a página 28px para baixo, o botão top será exibido
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 28 || document.documentElement.scrollTop > 28) {
-    mybutton.style.display = "block";
+    btnTop.style.display = "block";
   } else {
-    mybutton.style.display = "none";
+    btnTop.style.display = "none";
   }
 }
 
